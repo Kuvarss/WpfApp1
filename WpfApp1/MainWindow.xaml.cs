64f612569bddcd4c9380e8a46aa9b12d97195958
Kuvarss <inf2972@hs-worms.de>
2018-11-25 16:22:17 +0100

    Lyout fast fertig!

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -22,8 +22,42 @@ namespace WpfApp1
     {
         public MainWindow()
         {
-
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
 
+            //Button Click Close
+        private void bttnClose_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+            //Fenster verschieben
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        // Bildischrim maximieren und normalieren
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }else if(this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+        //  Bildschrim minimieren
+        private void bttnMinimze_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal || this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void TextBlock_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
     }
 }
